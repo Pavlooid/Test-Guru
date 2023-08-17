19f0e876b01746fa39b96d5eaf47f807ae480996
Pavlooid <pavlooo987@gmail.com>
2023-08-17 16:58:49 +0300

    Sixth lesson

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,27 @@
+class QuestionsController < ApplicationController
+  #before_action :test
+
+
+  def index
+    @questions = @test.questions
+  end
+
+  def start
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+end
