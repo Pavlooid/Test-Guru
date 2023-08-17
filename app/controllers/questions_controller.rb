ac324a7a3b0cc766c994e3a55a0d2c030ad5ff7d
Pavlooid <pavlooo987@gmail.com>
2023-08-17 17:58:20 +0300

    First work

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,27 +1,28 @@
-class QuestionsController < ApplicationController
-  #before_action :test
+# frozen_string_literal: true
 
+class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index]
+  bedore_action :find_question, only: %i[destroy]
 
   def index
     @questions = @test.questions
   end
 
-  def start
+  def start; end
 
-  end
+  def create; end
 
-  def create
+  def new; end
 
-  end
-
-  def new
-
-  end
-
-  def destroy
-
-  end
+  def destroy; end
 
   private
 
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
 end
