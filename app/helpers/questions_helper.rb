7344a70482d57c84a8b8bfb9872e4fda37169929
Pavlooid <pavlooo987@gmail.com>
2023-09-01 18:34:03 +0300

    Fixed issues 1

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
-  FOR_HEADERS = { 'new' => 'Создать новый', 'edit' => 'Изменить' }.freeze
-
-  def question_header(action, test_title)
-    header = "#{FOR_HEADERS[action]} вопрос по теме '#{test_title}'"
-    content_tag(:h1, header)
-  end
-
-  def question_headers(question)
+  def question_header(question)
     header = if question.new_record?
                "Создать новый вопрос по теме #{@test.title} "
              else
