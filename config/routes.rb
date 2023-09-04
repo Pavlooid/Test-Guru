6d992f5b91dc381f04a93a550199fecad598dbdf
Pavlooid <pavlooo987@gmail.com>
2023-09-04 17:12:29 +0300

    Raw code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'tests#index'
   resources :tests, shallow: true do
-    resources :questions, only: %i[show edit update destroy new create]
+    resources :questions, only: %i[show edit update destroy new create] do
+      resources :answers, shallow: true, except: :index
+    end
   end
 end
