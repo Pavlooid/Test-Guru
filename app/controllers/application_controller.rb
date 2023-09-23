76345a5149c162a7c860feefc91c9046460b8cf0
Pavlooid <pavlooo987@gmail.com>
2023-09-23 13:44:55 +0300

    Fixed grammar issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :configure_sugn_up_params, if: :devise_controller?
+  before_action :configure_sign_up_params, if: :devise_controller?
 
   def after_sign_in_path_for(_resource)
     flash[:notice] =  "Привет, #{current_user.first_name}!" unless current_user.is_a?(Admin)
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_sugn_up_params
+  def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
