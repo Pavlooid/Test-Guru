922e6c4039b19662f474931430a0fd1ffcf54b69
Pavlooid <pavlooo987@gmail.com>
2023-10-05 19:06:30 +0300

    Fixed issues 2

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -3,17 +3,16 @@ class GistsController < ApplicationController
     @test_passage = TestPassage.find(params[:test_passage_id])
     result = GistQuestionService.new(@test_passage.current_question).call
 
-    flash_options = if result.success?
+    if result.success?
       Gist.create!(
         question: @test_passage.current_question,
         url: result.url,
         email_of_author: current_user.email
       )
-      { notice: t('.success', link: view_context.link_to(t('.link'), result.url, target: "_blank")) }
+      redirect_to @test_passage, { notice: t('.success',
+      link: view_context.link_to(t('.link'), result.url, target: "_blank")) }
     else
-      { alert: t('.failure') }
+      redirect_to @test_passage, { alert: t('.failure') }
     end
-
-    redirect_to @test_passage, flash_options
   end
 end
