fb2271998cf51cc1c5c18f114611bb87134cca24
Pavlooid <pavlooo987@gmail.com>
2023-10-24 16:47:51 +0300

    another fixes 9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     tls:                  true,
-    adress:               'smtp.gmail.com',
+    address:              'smtp.gmail.com',
     port:                 587,
     domaim:               'gmail.com',
     user_name:            ENV['SMTP_USERNAME'],
