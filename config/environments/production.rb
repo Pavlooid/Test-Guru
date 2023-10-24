b3e3284296bb10280767bc1dd945a6ffb3fbd08a
Pavlooid <pavlooo987@gmail.com>
2023-10-24 16:21:46 +0300

    another fixes 7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     adress:               'smtp.gmail.com',
     port:                 587,
+    domaim:               'gmail.com',
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
